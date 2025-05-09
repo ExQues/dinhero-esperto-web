083c03bee9c879abbfbed6472448a1e750399db7
ExQues <harisson217@gmail.com>
2025-05-08 21:58:42 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider } from 'styled-components'; // ou uma solução similar
 
-createRoot(document.getElementById("root")!).render(<App />);
+const theme = {
+  light: {
+    background: 'white',
+    text: 'black',
+  },
+  dark: {
+    background: 'black',
+    text: 'white',
+  }
+};
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
