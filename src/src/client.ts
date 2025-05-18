c5dee56b5116f9e79ec21229ba96c41cb2cad838
ExQues <harisson217@gmail.com>
2025-05-18 23:55:14 +0000

    Add files via upload

---
+++ src/src/client.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from '../types';
+
+// Busca a URL e a chave anônima das variáveis de ambiente
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Verifica se as variáveis de ambiente foram carregadas corretamente
+if (!SUPABASE_URL || !SUPABASE_PUBLISHABLE_KEY) {
+  throw new Error("Supabase URL or Anon Key is missing. Make sure to set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY in your .env file or environment variables.");
+}
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
